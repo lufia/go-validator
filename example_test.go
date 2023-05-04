1b35ce7e571af8a2eaf7584f3062a2e93eaf6eac
lufia <lufia@lufia.org>
2023-05-04 18:44:42 +0900

    update example

--- example_test.go
+++ example_test.go
@@ -11,11 +11,13 @@ type Request struct {
 }
 
 type User struct {
+	ID   string
 	Name string
 }
 
 var requestValidator = requiring.Struct(func(s requiring.StructRuleAdder, r *Request) {
 	s.Add(&r.User, "user", requiring.Struct(func(s requiring.StructRuleAdder, u *User) {
+		s.Add(&u.ID, "id", requiring.Length[string](5, 10))
 		s.Add(&u.Name, "name", requiring.Required[string]())
 	}))
 })
@@ -24,5 +26,7 @@ func Example() {
 	var r Request
 	err := requestValidator.Validate(&r)
 	fmt.Println(err)
-	// Output: the field 'name' cannot be the zero value
+	// Unordered output:
+	// the field 'name' cannot be the zero value
+	// the length of the field 'id' must be in range(5 ... 10)
 }
